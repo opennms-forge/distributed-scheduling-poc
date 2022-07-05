9347f7a4cc41134418686b5c96a5590817796481
mfrazier <mark.frazier@contractor.cengage.com>
2022-07-05 11:14:31 -0700

    logging instance creation

--- ignite-worker/src/main/java/org/opennms/poc/ignite/worker/ignite/detectors/StaticDetectorRegistry.java
+++ ignite-worker/src/main/java/org/opennms/poc/ignite/worker/ignite/detectors/StaticDetectorRegistry.java
@@ -1,8 +1,10 @@
 package org.opennms.poc.ignite.worker.ignite.detectors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ignite.Ignite;
 import org.osgi.framework.BundleContext;
 
+@Slf4j
 public class StaticDetectorRegistry {
     private static DetectorRegistry detectorRegistry;
 
@@ -11,6 +13,7 @@ public class StaticDetectorRegistry {
     }
 
     public static void init(BundleContext bundleContext, Ignite ignite) {
+        log.info("Creating an instance of the StaticDetectorRegistry for initialization. Don't do this twice!");
         detectorRegistry = new DetectorRegistryImpl(bundleContext, ignite);
     }
 
