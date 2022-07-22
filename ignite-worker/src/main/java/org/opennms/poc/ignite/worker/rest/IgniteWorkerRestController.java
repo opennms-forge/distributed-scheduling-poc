ff0fe1455c178853764a47bd9e7b9c17d87352a4
Arthur Naseef <art@artnaseef.com>
2022-07-22 15:30:31 -0700

    Remove unused endpoints, "load-em-up", repeated-services, and no-op services (from early ignite prototyping).

--- ignite-worker/src/main/java/org/opennms/poc/ignite/worker/rest/IgniteWorkerRestController.java
+++ ignite-worker/src/main/java/org/opennms/poc/ignite/worker/rest/IgniteWorkerRestController.java
@@ -1,10 +1,7 @@
 package org.opennms.poc.ignite.worker.rest;
 
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import org.opennms.poc.ignite.model.workflows.Network;
 
 @Path("/poc")
 public interface IgniteWorkerRestController {
@@ -20,24 +17,4 @@ public interface IgniteWorkerRestController {
     @GET
     @Path("/hi-all")
     void hiAll();
-
-    @GET
-    @Path("/hi-all-repeated-service")
-    void deployHiAllRepeatedService();
-
-    @DELETE
-    @Path("/hi-all-repeated-service")
-    void removeHiAllRepeatedService();
-
-    @GET
-    @Path("/noop-service/{count}")
-    String deployNoopService(@PathParam("count") int count);
-
-    @DELETE
-    @Path("/noop-service/{count}")
-    String undeployNoopService(@PathParam("count") int count);
-
-    @GET
-    @Path("/load-em-up")
-    public void loadEmUp(Network.NetworkSize size);
 }
