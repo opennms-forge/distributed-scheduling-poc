00f79c4731a4685b8ed92d5154ae94ff66cd5c56
Arthur Naseef <art@artnaseef.com>
2022-07-25 14:46:48 -0700

    Add Test Tcp Connection server to test-driver.

--- scheduler/src/main/java/org/opennms/poc/scheduler/impl/OpennmsSchedulerImpl.java
+++ scheduler/src/main/java/org/opennms/poc/scheduler/impl/OpennmsSchedulerImpl.java
@@ -2,7 +2,6 @@ package org.opennms.poc.scheduler.impl;
 
 import com.cronutils.model.Cron;
 import com.cronutils.model.CronType;
-import com.cronutils.model.definition.CronDefinition;
 import com.cronutils.model.definition.CronDefinitionBuilder;
 import com.cronutils.model.time.ExecutionTime;
 import com.cronutils.parser.CronParser;
@@ -14,7 +13,6 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.time.ZonedDateTime;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
