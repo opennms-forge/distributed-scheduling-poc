edb4036c67fb4bc433e7e26565052a1c5f8f7f6f
Arthur Naseef <art@artnaseef.com>
2022-07-27 08:40:19 -0700

    Adjust logged output for load.  Update post-workflow to work with larger data files.

--- test-driver/src/main/java/org/opennms/poc/testdriver/workflow/LoggingResultCollector.java
+++ test-driver/src/main/java/org/opennms/poc/testdriver/workflow/LoggingResultCollector.java
@@ -1,6 +1,8 @@
 package org.opennms.poc.testdriver.workflow;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.opennms.poc.ignite.grpc.workflow.contract.WorkflowResults.WorkflowResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +11,24 @@ public class LoggingResultCollector implements ResultCollector {
 
   private final Logger logger = LoggerFactory.getLogger(LoggingResultCollector.class);
 
+  private AtomicLong samplesCounted = new AtomicLong(0);
+
+
   @Override
   public void process(WorkflowResult result, Map<String, Object> resultMap) {
-    logger.info("Received result workflow={} status={} reason={} params={}", result.getUuid(),
-        result.getStatus(), result.getReason(), resultMap);
+    long sampleCount = samplesCounted.incrementAndGet();
+    if (sampleCount < 20) {
+      logger.info("Sample count received " + sampleCount);
+      logger.info("Received result workflow={} status={} reason={} params={}", result.getUuid(),
+              result.getStatus(), result.getReason(), resultMap);
+    } else if ((sampleCount < 1000) && ((sampleCount % 100) == 0)) {
+      logger.info("..Sample count received " + sampleCount);
+    } else if ((sampleCount < 10000) && ((sampleCount % 1000) == 0)) {
+      logger.info("...Sample count received " + sampleCount);
+    } else if ((sampleCount % 10000) == 0) {
+      logger.info("....Sample count received " + sampleCount);
+    }
+
   }
 
 }
