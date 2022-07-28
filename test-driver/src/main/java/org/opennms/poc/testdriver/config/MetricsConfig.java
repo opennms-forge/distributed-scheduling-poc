4b5eba544b4c1f2f811293be25aae19ace681550
Łukasz Dywicki <luke@code-house.org>
2022-07-28 19:42:59 +0200

    Introduce prometheus scrap endpoint.
    Signed-off-by: Łukasz Dywicki <luke@code-house.org>

--- test-driver/src/main/java/org/opennms/poc/testdriver/config/MetricsConfig.java
+++ test-driver/src/main/java/org/opennms/poc/testdriver/config/MetricsConfig.java
@@ -3,21 +3,11 @@ package org.opennms.poc.testdriver.config;
 import com.codahale.metrics.MetricRegistry;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.dropwizard.DropwizardExports;
-import io.prometheus.client.exporter.PushGateway;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MetricsConfig {
-
-  @Value("${push-gateway:push-gateway:8080}")
-  private String pushGateway;
-
-  @Bean
-  public PushGateway pushGateway() {
-    return new PushGateway(pushGateway);
-  }
 
   @Bean
   public MetricRegistry metricRegistry() {
