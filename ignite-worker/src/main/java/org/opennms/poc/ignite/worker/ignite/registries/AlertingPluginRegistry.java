22e9fd341eeb230a63e965053f81328b797b55ed
mfrazier <mfrazier@savoirtech.com>
2022-08-01 16:37:57 -0700

    First cut at collecting and aggregating plugin config information, protobuf message for horizon

--- ignite-worker/src/main/java/org/opennms/poc/ignite/worker/ignite/registries/AlertingPluginRegistry.java
+++ ignite-worker/src/main/java/org/opennms/poc/ignite/worker/ignite/registries/AlertingPluginRegistry.java
@@ -3,16 +3,23 @@ package org.opennms.poc.ignite.worker.ignite.registries;
 import com.savoirtech.eos.pattern.whiteboard.KeyedWhiteboard;
 import com.savoirtech.eos.util.ServiceProperties;
 import java.util.HashMap;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.ProducerTemplate;
+import org.opennms.poc.ignite.model.workflows.WorkflowType;
+import org.opennms.poc.plugin.api.FieldConfigMeta;
+import org.opennms.poc.plugin.config.PluginConfigScanner;
+import org.opennms.poc.plugin.config.PluginMetadata;
 import org.osgi.framework.BundleContext;
 
 @Slf4j
 public class AlertingPluginRegistry<K, S> extends KeyedWhiteboard<K, S>  {
-    ProducerTemplate producerTemplate;
+    private final ProducerTemplate producerTemplate;
+    private final String id;
 
     public AlertingPluginRegistry(BundleContext bundleContext, Class<S> serviceType, String id, ProducerTemplate producerTemplate) {
         super(bundleContext, serviceType, (svc, props) -> props.getProperty(id));
+        this.id = id;
         this.producerTemplate = producerTemplate;
     }
 
@@ -20,8 +27,11 @@ public class AlertingPluginRegistry<K, S> extends KeyedWhiteboard<K, S>  {
     protected K addService(S service, ServiceProperties props) {
         K retVal = super.addService(service, props);
 
-        //TODO: need to know what we are sending here
-        producerTemplate.sendBodyAndHeaders("blahMessage", new HashMap<>());
+        List<FieldConfigMeta> fieldConfigMetaList = PluginConfigScanner.getConfigs(service.getClass());
+        //TODO: make this is the impl class, not the interface
+        log.info("################# Performing scan on service {}", service.getClass());
+        PluginMetadata pluginMetadata = new PluginMetadata(props.getProperty(id), WorkflowType.DETECTOR, fieldConfigMetaList);
+        producerTemplate.sendBody(pluginMetadata);
 
         return retVal;
     }
--- ignite-worker/src/main/java/org/opennms/poc/routing/MinionRouting.java
+++ ignite-worker/src/main/java/org/opennms/poc/routing/MinionRouting.java
@@ -1,32 +1,63 @@
 package org.opennms.poc.routing;
 
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.aggregate.AbstractListAggregationStrategy;
+import org.opennms.horizon.ipc.sink.api.MessageDispatcherFactory;
+import org.opennms.horizon.ipc.sink.api.SyncDispatcher;
+import org.opennms.poc.ignite.grpc.workflow.contract.WorkflowResults;
+import org.opennms.poc.plugin.config.PluginMetadata;
+import orh.opennms.poc.ignite.grpc.workflow.WorkflowSinkModule;
 
 @Slf4j
 public class MinionRouting extends RouteBuilder {
 
-    public final String routeUri;
+    private final String routeUri;
     public static final String ROUTE_ID =  "MINION_REGISTRATION";
+//    private final SyncDispatcher<PluginConfig> dispatcher;
 
-    public MinionRouting(String uri) {
+
+    //TODO: make this configurable
+    private final long someDelay=10000;
+
+    public MinionRouting(String uri, MessageDispatcherFactory messageDispatcherFactory) {
         this.routeUri = uri;
+//        dispatcher = messageDispatcherFactory.createSyncDispatcher(new WorkflowSinkModule());
     }
 
     @Override
     public void configure() throws Exception {
         from(routeUri).routeId(ROUTE_ID).
-        process(new Processor() {
-            @Override
-            public void process(Exchange exchange) throws Exception {
-                log.info("Got a plugin registration notice!");
-                //TODO: send message to horizon
-                //TODO: Should probably aggregate all registrations into one single message for minion registration with horizon
-            }
-        });
+                log(LoggingLevel.INFO, "Got a single plugin config message").
+                aggregate(new PluginConfigAggregationStrategy()).constant(true).
+                completionTimeout(someDelay).
+                process(exchange -> {
+                    log.info("Got a plugin registration notice!");
+
+                    List<PluginMetadata> pluginMetadataList = exchange.getIn().getBody(List.class);
+
+                    log.info("Got {} configs", pluginMetadataList.size());
+
+                    // now get the builder for the protobuf message and contruct it from the PluginMetadata
+
+//                    PluginConfig pluginConfig = ????
+//                    dispatcher.send(pluginConfig);
+                });
 
         //TODO: we may need dead letter handling here if comms to horizon haven't spun up yet.
+    }
+
+    private class PluginConfigAggregationStrategy extends AbstractListAggregationStrategy<PluginMetadata> {
+
+        @Override
+        public PluginMetadata getValue(Exchange exchange) {
+            return exchange.getIn().getBody(PluginMetadata.class);
+        }
     }
 }
