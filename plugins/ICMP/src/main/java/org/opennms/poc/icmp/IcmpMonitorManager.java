e10dbd76dc80223838aa3c68dde322fa65b2f514
mfrazier <mfrazier@savoirtech.com>
2022-08-01 09:19:32 -0700

    Fixing blueprint wiring

--- plugins/ICMP/src/main/java/org/opennms/poc/icmp/IcmpMonitorManager.java
+++ plugins/ICMP/src/main/java/org/opennms/poc/icmp/IcmpMonitorManager.java
@@ -1,14 +1,16 @@
 package org.opennms.poc.icmp;
 
 import java.util.function.Consumer;
+import lombok.RequiredArgsConstructor;
 import org.opennms.netmgt.icmp.PingerFactory;
 import org.opennms.poc.plugin.api.ServiceMonitor;
 import org.opennms.poc.plugin.api.ServiceMonitorManager;
 import org.opennms.poc.plugin.api.ServiceMonitorResponse;
 import org.opennms.poc.plugin.api.annotations.HorizonConfig;
 
+@RequiredArgsConstructor
 public class IcmpMonitorManager implements ServiceMonitorManager {
-    PingerFactory pingerFactory;
+    private final PingerFactory pingerFactory;
 
     @HorizonConfig(displayName = "hypotheticalConfig")
     public String moreConfig;
